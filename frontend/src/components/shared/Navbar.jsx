54ac60b8bf076443a35079da5249c8f2110f2356
Poojanpatel2003 <poojanpatel1595@gmail.com>
2025-02-04 22:16:43 +0530

    optimizing Development

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,133 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
+import { Button } from "../ui/button";
+import { LogOut, User2 } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "sonner";
+import axios from "axios";
+import { USER_API_END_POINT } from "@/utils/constant";
+import { setUser } from "@/redux/authSlice";
+
+const Navbar = () => {
+  const { user } = useSelector((store) => store.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get(`${USER_API_END_POINT}/logout`, {
+        withCredentials: true,
+      });
+      if (res.data.SUCCESS) {
+        dispatch(setUser(null));
+        navigate("/");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+  };
+  return (
+    <div className="bg-white my-5" >
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            {/* Job <span className="text-[#F83002]">Portal</span> */}
+            <img src="/logo.png" alt="JobPortal" style={{width:'250px'}} />
+          </h1>
+        </div>
+        <div className="flex items-center gap-5">
+          <ul className="flex font-medium items-center gap-5">
+            {user && user.role === "recruiter" ? (
+              <>
+                {" "}
+                <li>
+                  <Link to="/admin/companies">Companies</Link>
+                </li>
+                <li>
+                  <Link to="/admin/jobs">jobs</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/jobs">jobs</Link>
+                </li>
+                <li>
+                  <Link to="/browse">Browse</Link>
+                </li>
+              </>
+            )}
+          </ul>
+          {!user ? (
+            <div className="flex items-center gap-2">
+              <Link to="/login">
+                <Button variant="outline">Login</Button>
+              </Link>
+              <Link to="/signup">
+                <Button className="bg-[#6A38C2]   hover:bg-[#5B30a6]">
+                  SignUp
+                </Button>
+              </Link>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger asChild>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage
+                    src={user?.profile?.profilePhoto}
+                    alt="@shadcn"
+                    style={{ borderRadius: "50%", width: "40px" }}
+                  />
+                </Avatar>
+              </PopoverTrigger>
+              <PopoverContent className="w-70 rounded-lg border border-dark-500 p-3 mt-2">
+                <div className="flex items-center gap-4  p-3">
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage
+                      src={user?.profile?.profilePhoto}
+                      alt="@shadcn"
+                      style={{ borderRadius: "50%", width: "40px" }}
+                    />
+                  </Avatar>
+                  <div>
+                    <h4 className="font-medium">{user?.fullname}</h4>
+                    <p>{user?.profile?.bio}</p>
+                  </div>
+                </div>
+                <div className="flex gap-3 text-grey-600 ">
+                  {user && user.role === "student" && (
+                    <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                     
+                      <Button variant="Link" className="bg-slate-200">
+                      <User2 /> 
+                        <Link to="profile">View Profile</Link>
+                      </Button>
+                    </div>
+                  )}
+
+                  <Button
+                    onClick={logoutHandler}
+                    variant="Link"
+                    className="bg-red-600 text-white"
+                  >
+                    Logout <LogOut />
+                  </Button>
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
